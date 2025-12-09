95af7808e1069e6eadd9d4ef59d67269c91f00e1
Mihitha01 <mihithabandara@gmail.com>
2025-12-09 21:42:18 +0530

    update database password in settings.py

--- smartagri_backend/settings.py
+++ smartagri_backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'smartagrimarket',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'bankaimysql2001',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
