a475e0e244b43f8c7bc422f0763b88fb704e4ce3
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2025-12-26 11:32:27 +0530

    update

--- smartagri_backend/urls.py
+++ smartagri_backend/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('api/', include('buyer.urls')),
     path('api/order/', include('order.urls')),
     path('api/documents/', include('documents.urls')),
-    path('api/reviews/', include('reviews.urls'))
+    path('api/reviews/', include('reviews.urls')),
     path("api/farmer/", include("user.urls"))
 ]
