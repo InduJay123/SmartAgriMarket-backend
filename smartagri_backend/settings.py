1a2cc73b1e28543e5f48d6b26fabad1d182187b2
Mihitha01 <mihithabandara@gmail.com>
2026-03-08 22:28:56 +0530

    refactor: reorganize crop and marketplace migrations, update database password

--- smartagri_backend/settings.py
+++ smartagri_backend/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'smartagrimarket',
         'USER': 'root',
-        'PASSWORD': 'bankaimysql',
+        'PASSWORD': 'bankaimysql2001',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
